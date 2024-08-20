2f56dbe5c875fe75242ca18c8b5c266e97d88d8f
Chethy100 <canoroncreative@gmail.com>
2024-08-21 03:02:37 +0530

    Delete Thavishka Site directory

--- Thavishka Site/function.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navLinks = document.getElementById('nav-links');
-    
-    // Toggle the active class to show/hide the menu
-    navLinks.classList.toggle('active');
-});
